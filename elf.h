64085fd77223a5f82bb319fdbd0c60f5a66234ff
Samarth Patel <cs1230844@iitd.ac.in>
2026-03-12 09:12:19 +0530

    Removed dead code

--- elf.h
+++ elf.h
@@ -32,11 +32,3 @@ struct proghdr {
   uint flags;
   uint align;
 };
-
-// Values for Proghdr type
-#define ELF_PROG_LOAD           1
-
-// Flag bits for Proghdr flags
-#define ELF_PROG_FLAG_EXEC      1
-#define ELF_PROG_FLAG_WRITE     2
-#define ELF_PROG_FLAG_READ      4
